b308cadc149a5b50d1b2574f21f6dbf18fe6d135
Ewgenii Voloshyn <enejmail@gmail.com>
2016-06-17 06:12:02 +0300

    Update sellerconfig.php

--- upload/system/library/sellerconfig.php
+++ upload/system/library/sellerconfig.php
@@ -1,31 +1,6 @@
 <?php
 class Sellerconfig {
-	private $data = array();
-
-	public function get($key) {
-		return (isset($this->data[$key]) ? $this->data[$key] : null);
-	}
-
-	public function set($key, $value) {
-		$this->data[$key] = $value;
-	}
-
-	public function has($key) {
-		return isset($this->data[$key]);
-	}
-
-	public function load($filename) {
-		$file = DIR_CONFIG . $filename . '.php';
-
-		if (file_exists($file)) {
-			$_ = array();
-
-			require($file);
-
-			$this->data = array_merge($this->data, $_);
-		} else {
-			trigger_error('Error: Could not load config ' . $filename . '!');
-			exit();
-		}
-	}
+   public function __construct($registry) {
+   $this->sellerconfig = $registry->get('sellerconfig');	
+   }
 }
