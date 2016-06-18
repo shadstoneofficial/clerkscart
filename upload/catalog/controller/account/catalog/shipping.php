d8afef714b8e067a43d3ba73792006230810dafa
Ewgenii Voloshyn <enejmail@gmail.com>
2016-06-19 01:01:14 +0300

    Add files via upload

---
+++ upload/catalog/controller/account/catalog/shipping.php
@@ -0,0 +1,108 @@
+<?php
+class ControllerAccountCatalogShipping extends Controller {
+	private $error = array();
+
+	public function index() {
+  $seller_id = $this->customer->getId();
+    if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/catalog/shipping', '', true);
+
+			$this->response->redirect($this->url->link('account/login', '', true));
+		} else if ($this->customer->hasSellerPermission($seller_id) == 0) {
+			$this->session->data['redirect'] = $this->url->link('account/catalog/shipping', 'token=' . $this->session->data['token'], true);
+
+			$this->response->redirect($this->url->link('account/account', 'token=' . $this->session->data['token'], true));
+		}
+		$this->load->language('account/catalog/shipping');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->load->model('extension/extension');
+
+		$this->getList();
+	}
+
+	public function getList() {
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('account/account', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('account/catalog/shipping', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['text_list'] = $this->language->get('text_list');
+		$data['text_no_results'] = $this->language->get('text_no_results');
+		$data['text_confirm'] = $this->language->get('text_confirm');
+
+		$data['column_name'] = $this->language->get('column_name');
+		$data['column_status'] = $this->language->get('column_status');
+		$data['column_sort_order'] = $this->language->get('column_sort_order');
+		$data['column_action'] = $this->language->get('column_action');
+
+		$data['button_edit'] = $this->language->get('button_edit');
+		$data['button_install'] = $this->language->get('button_install');
+		$data['button_uninstall'] = $this->language->get('button_uninstall');
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		if (isset($this->session->data['success'])) {
+			$data['success'] = $this->session->data['success'];
+
+			unset($this->session->data['success']);
+		} else {
+			$data['success'] = '';
+		}
+
+		$this->load->model('extension/extension');
+
+		$extensions = $this->model_extension_extension->getInstalled('shipping');
+
+		$data['extensions'] = array();
+
+		$files = glob(DIR_APPLICATION . 'controller/account/shipping/*.php');
+
+		if ($files) {
+			foreach ($files as $file) {
+				$extension = basename($file, '.php');
+
+				$this->load->language('shipping/' . $extension);
+
+				$data['extensions'][] = array(
+					'name'       => $this->language->get('heading_title'),
+					'status'     => $this->config->get($extension . '_status') ? $this->language->get('text_enabled') : $this->language->get('text_disabled'),
+					'sort_order' => $this->config->get($extension . '_sort_order'),
+					'installed'  => in_array($extension, $extensions),
+					'edit'       => $this->url->link('shipping/' . $extension, 'token=' . $this->session->data['token'], true)
+				);
+			}
+		}
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		$this->response->setOutput($this->load->view('account/catalog/shipping', $data));
+	}
+
+	protected function validate() {
+		$seller_id = $this->customer->getId();
+    if ($this->customer->hasSellerPermission($seller_id) == 0) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+		return !$this->error;
+ }
+}
