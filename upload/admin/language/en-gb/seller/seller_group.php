17bd003299312e2f007c63c15a36930efea1581c
Ewgenii Voloshyn <enejmail@gmail.com>
2016-07-10 15:48:21 +0300

    Add files via upload

---
+++ upload/admin/language/en-gb/seller/seller_group.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']     = 'Seller Groups';
+
+// Text
+$_['text_success']      = 'Success: You have modified seller groups!';
+$_['text_list']         = 'Seller Group List';
+$_['text_add']          = 'Add Seller Group';
+$_['text_edit']         = 'Edit Seller Group';
+
+// Column
+$_['column_name']       = 'Seller Group Name';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Seller Group Name';
+$_['entry_description'] = 'Description';
+$_['entry_approval']    = 'Approve New Sellers';
+$_['entry_sort_order']  = 'Sort Order';
+$_['entry_prodlimit']   = 'Max product qty.';
+$_['entry_imglimit']    = 'Max disk size for filemanager (MB)';
+$_['entry_downloadlimit']  = 'Max disk size for downloads (MB)';
+
+// Help
+$_['help_approval']     = 'Sellers must be approved by an administrator before they can login.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify seller groups!';
+$_['error_name']         = 'Seller Group Name must be between 3 and 32 characters!';
+$_['error_default']      = 'Warning: This seller group cannot be deleted as it is currently assigned as the default store seller group!';
+$_['error_store']        = 'Warning: This seller group cannot be deleted as it is currently assigned to %s stores!';
+$_['error_seller']     = 'Warning: This seller group cannot be deleted as it is currently assigned to %s sellers!';
