e7f94dd5f212880eee4713b5a73bed751c8eafad
Ewgenii Voloshyn <enejmail@gmail.com>
2016-07-11 15:28:42 +0300

    Add files via upload

---
+++ upload/catalog/model/account/catalog/seller_group.php
@@ -0,0 +1,14 @@
+<?php
+class ModelAccountCatalogSellerGroup extends Model {
+	public function getSellerGroup($seller_group_id) {
+		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "seller_group cg LEFT JOIN " . DB_PREFIX . "seller_group_description cgd ON (cg.seller_group_id = cgd.seller_group_id) WHERE cg.seller_group_id = '" . (int)$seller_group_id . "' AND cgd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+
+		return $query->row;
+	}
+
+	public function getSellerGroups() {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seller_group cg LEFT JOIN " . DB_PREFIX . "seller_group_description cgd ON (cg.seller_group_id = cgd.seller_group_id) WHERE cgd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY cg.sort_order ASC, cgd.name ASC");
+
+		return $query->rows;
+	}
+}
