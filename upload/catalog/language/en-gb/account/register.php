ac77017bff5421e2ed43853133201defe479923b
Ewgenii Voloshyn <enejmail@gmail.com>
2016-07-11 15:58:18 +0300

    Update register.php

--- upload/catalog/language/en-gb/account/register.php
+++ upload/catalog/language/en-gb/account/register.php
@@ -14,6 +14,7 @@ $_['text_agree']                  = 'I have read and agree to the <a href="%s" c
 
 // Entry
 $_['entry_customer_group']        = 'Customer Group';
+$_['entry_seller_group']          = 'Seller Group';
 $_['entry_firstname']             = 'First Name';
 $_['entry_lastname']              = 'Last Name';
 $_['entry_email']                 = 'E-Mail';
