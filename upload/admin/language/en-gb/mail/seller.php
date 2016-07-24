739ce1754267cb037eb6a3bded65d8194402b2f5
Ewgenii Voloshyn <enejmail@gmail.com>
2016-07-24 04:07:19 +0300

    Add files via upload

---
+++ upload/admin/language/en-gb/mail/seller.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_seller_approve_subject']      = '%s - You are approved as a seller!';
+$_['text_approve_welcome']      = 'Welcome and thank you for registering at %s!';
+$_['text_approve_login']        = 'You can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_approve_services']     = 'Upon logging in, you will be able to saccess other services including - add product, reviewing past orders, printing invoices and editing your account information as a seller.';
+$_['text_approve_thanks']       = 'Thanks,';
+$_['text_transaction_subject']  = '%s - Account Credit';
+$_['text_transaction_received'] = 'You have received %s credit!';
+$_['text_transaction_total']    = 'Your total amount of credit is now %s.' . "\n\n" . 'Your account credit will be automatically deducted from your next purchase.';
+$_['text_reward_subject']       = '%s - Reward Points';
+$_['text_reward_received']      = 'You have received %s Reward Points!';
+$_['text_reward_total']         = 'Your total number of reward points is now %s.';
