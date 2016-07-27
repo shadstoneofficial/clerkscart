a9f3ce1fa0337909f085103a3e228b513f15a1b1
Ewgenii Voloshyn <enejmail@gmail.com>
2016-07-27 15:51:32 +0300

    Update order.php

--- upload/catalog/model/checkout/order.php
+++ upload/catalog/model/checkout/order.php
@@ -738,9 +738,9 @@ class ModelCheckoutOrder extends Model {
 						$text .= $order_info['comment'] . "\n\n";
 					}
 	
-          $seller_id = $order_info['seller_id'];
-          
-          $seller_mail = $this->db->query("SELECT email FROM `" . DB_PREFIX . "seller` WHERE seller_id = '" . (int)$seller_id . "'");
+                                        $seller_id = $order_info['seller_id'];      
+                                        $this->load->model('account/catalog/seller');      
+                                        $seller_info = $this->model_account_catalog_seller->getSeller($seller_id);
           
 					$mail = new Mail();
 					$mail->protocol = $this->config->get('config_mail_protocol');
@@ -751,7 +751,7 @@ class ModelCheckoutOrder extends Model {
 					$mail->smtp_port = $this->config->get('config_mail_smtp_port');
 					$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 	
-					$mail->setTo($seller_mail);
+					$mail->setTo($seller_info['email']);
 					$mail->setFrom($this->config->get('config_email'));
 					$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
 					$mail->setSubject(html_entity_decode($subject, ENT_QUOTES, 'UTF-8'));
