af680615faf1fe7d7d138350ad1d4d4cdd7e46e8
Ewgenii Voloshyn <enejmail@gmail.com>
2016-08-08 17:42:43 +0300

    Update return.php

--- upload/catalog/controller/account/return.php
+++ upload/catalog/controller/account/return.php
@@ -618,7 +618,7 @@ class ControllerAccountReturn extends Controller {
 
 		$data['button_continue'] = $this->language->get('button_continue');
 
-		$data['continue'] = $this->url->link('common/home');
+		$data['continue'] = $this->url->link('account/account');
 
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
