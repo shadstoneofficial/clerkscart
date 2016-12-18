8983427b0abbc4a3abed0815f5cf05a7f521efd5
Ewgenii Voloshyn <enejmail@gmail.com>
2016-12-18 22:13:53 +0200

    Update cart.php

--- upload/catalog/language/en-gb/common/cart.php
+++ upload/catalog/language/en-gb/common/cart.php
@@ -5,3 +5,4 @@ $_['text_empty']     = 'Your shopping cart is empty!';
 $_['text_cart']      = 'View Cart';
 $_['text_checkout']  = 'Checkout';
 $_['text_recurring'] = 'Payment Profile';
+$_['seller_name'] = 'Seller';
