18a7423be559dd40cc1f8f438478280f9992f1f5
Ewgenii Voloshyn <enejmail@gmail.com>
2016-12-23 23:19:30 +0200

    Update customer.php

--- upload/system/library/cart/customer.php
+++ upload/system/library/cart/customer.php
@@ -170,6 +170,47 @@ class Customer {
 		}	
 	}
 	
+	public function getSellers($data = array()) {
+		$sql = "SELECT * FROM " . DB_PREFIX . "seller";
+
+		if (!empty($data['filter_name'])) {
+			$sql .= " WHERE company LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+		}
+
+		$sort_data = array(
+			'name',
+			'sort_order'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY name";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+	
 	public function getRewardPoints() {
 		$query = $this->db->query("SELECT SUM(points) AS total FROM " . DB_PREFIX . "customer_reward WHERE customer_id = '" . (int)$this->customer_id . "'");
 
