eb63ba899602dd6515bacdaadaaaa99614be3efb
Ewgenii Voloshyn <enejmail@gmail.com>
2017-01-05 02:47:53 +0200

    Update 227.php

--- upload/install/model/upgrade/227.php
+++ upload/install/model/upgrade/227.php
@@ -1,7 +1,10 @@
 <?php
 class ModelUpgrade227 extends Model {
 	public function upgrade() {
-		//Voucher
-		$this->db->query("ALTER TABLE `" . DB_PREFIX . "voucher` ADD COLUMN `seller_id` int(11) NOT NULL AFTER `voucher_id`");		
-	}
+	//Voucher
+    	$query = $this->db->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '" . DB_DATABASE . "' AND TABLE_NAME = '" . DB_PREFIX . "voucher' AND COLUMN_NAME = 'seller_id'");
+          if (!$query->num_rows) {
+	$this->db->query("ALTER TABLE `" . DB_PREFIX . "voucher` ADD COLUMN `seller_id` int(11) NOT NULL AFTER `voucher_id`");
+          }		
+  }
 }
