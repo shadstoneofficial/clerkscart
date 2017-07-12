1450439b01482fbe549cac3ad33fa0d2f381884d
Ewgenii Voloshyn <enejmail@gmail.com>
2017-07-12 10:44:17 +0300

    Update common.js

--- upload/admin/view/javascript/common.js
+++ upload/admin/view/javascript/common.js
@@ -149,7 +149,7 @@ $(document).ready(function() {
 									
 									$('#modal-image').modal('show');
 									
-									$('#modal-image a.thumbnail').on('click', function(e) {
+									$('#modal-image').on('click', 'a.thumbnail', function(e) {
 										e.preventDefault();
 										
 										$(element).summernote('insertImage', $(this).attr('href'));
