d83152449c0147a0b823360e3922254dfd234506
Ewgenii Voloshyn <enejmail@gmail.com>
2017-09-28 03:24:12 +0300

    Update customer.php

--- upload/admin/model/report/customer.php
+++ upload/admin/model/report/customer.php
@@ -315,7 +315,29 @@ class ModelReportCustomer extends Model {
 
 		return $query->row['total'];
 	}
+        
+	  	public function getTotalSellersOnline($data = array()) {
+		$sql = "SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "customer_online` co LEFT JOIN " . DB_PREFIX . "customer c ON (co.customer_id = c.customer_id)";
 
+		$implode = array();
+
+		if (!empty($data['filter_ip'])) {
+			$implode[] = "co.ip LIKE '" . $this->db->escape($data['filter_ip']) . "'";
+		}
+
+		if (!empty($data['filter_customer'])) {
+			$implode[] = "co.customer_id > 0 AND CONCAT(c.firstname, ' ', c.lastname) LIKE '" . $this->db->escape($data['filter_customer']) . "'";
+		}
+
+		if ($implode) {
+			$sql .= " WHERE " . implode(" AND ", $implode);
+		}
+    $sql .= " AND sellerapprove = '1'";
+		$query = $this->db->query($sql);
+
+		return $query->row['total'];
+	}
+	
 	public function getCustomerActivities($data = array()) {
 		$sql = "SELECT ca.customer_activity_id, ca.customer_id, ca.key, ca.data, ca.ip, ca.date_added FROM " . DB_PREFIX . "customer_activity ca LEFT JOIN " . DB_PREFIX . "customer c ON (ca.customer_id = c.customer_id)";
 
