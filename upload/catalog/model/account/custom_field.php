c5673be657fb154049253552dec52c30a4718474
Ewgenii Voloshyn <enejmail@gmail.com>
2017-11-10 06:57:25 +0200

    Update custom_field.php

--- upload/catalog/model/account/custom_field.php
+++ upload/catalog/model/account/custom_field.php
@@ -44,4 +44,66 @@ class ModelAccountCustomField extends Model {
 
 		return $custom_field_data;
 	}
+	  	public function getCustomFieldDescriptions($custom_field_id) {
+		$custom_field_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "custom_field_description WHERE custom_field_id = '" . (int)$custom_field_id . "'");
+
+		foreach ($query->rows as $result) {
+			$custom_field_data[$result['language_id']] = array('name' => $result['name']);
+		}
+
+		return $custom_field_data;
+	}
+	
+	public function getCustomFieldValue($custom_field_value_id) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "custom_field_value cfv LEFT JOIN " . DB_PREFIX . "custom_field_value_description cfvd ON (cfv.custom_field_value_id = cfvd.custom_field_value_id) WHERE cfv.custom_field_value_id = '" . (int)$custom_field_value_id . "' AND cfvd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+
+		return $query->row;
+	}
+	
+	public function getCustomFieldValues($custom_field_id) {
+		$custom_field_value_data = array();
+
+		$custom_field_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "custom_field_value cfv LEFT JOIN " . DB_PREFIX . "custom_field_value_description cfvd ON (cfv.custom_field_value_id = cfvd.custom_field_value_id) WHERE cfv.custom_field_id = '" . (int)$custom_field_id . "' AND cfvd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY cfv.sort_order ASC");
+
+		foreach ($custom_field_value_query->rows as $custom_field_value) {
+			$custom_field_value_data[$custom_field_value['custom_field_value_id']] = array(
+				'custom_field_value_id' => $custom_field_value['custom_field_value_id'],
+				'name'                  => $custom_field_value['name']
+			);
+		}
+
+		return $custom_field_value_data;
+	}
+	
+	public function getCustomFieldCustomerGroups($custom_field_id) {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "custom_field_customer_group` WHERE custom_field_id = '" . (int)$custom_field_id . "'");
+
+		return $query->rows;
+	}
+
+	public function getCustomFieldValueDescriptions($custom_field_id) {
+		$custom_field_value_data = array();
+
+		$custom_field_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "custom_field_value WHERE custom_field_id = '" . (int)$custom_field_id . "'");
+
+		foreach ($custom_field_value_query->rows as $custom_field_value) {
+			$custom_field_value_description_data = array();
+
+			$custom_field_value_description_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "custom_field_value_description WHERE custom_field_value_id = '" . (int)$custom_field_value['custom_field_value_id'] . "'");
+
+			foreach ($custom_field_value_description_query->rows as $custom_field_value_description) {
+				$custom_field_value_description_data[$custom_field_value_description['language_id']] = array('name' => $custom_field_value_description['name']);
+			}
+
+			$custom_field_value_data[] = array(
+				'custom_field_value_id'          => $custom_field_value['custom_field_value_id'],
+				'custom_field_value_description' => $custom_field_value_description_data,
+				'sort_order'                     => $custom_field_value['sort_order']
+			);
+		}
+
+		return $custom_field_value_data;
+	}
 }
