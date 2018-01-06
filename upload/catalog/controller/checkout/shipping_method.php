690ec9d73aab11e1a55c9c410a9554b5a028c511
Ewgenii Voloshyn <enejmail@gmail.com>
2018-01-06 06:28:19 +0200

    Update shipping_method.php

--- upload/catalog/controller/checkout/shipping_method.php
+++ upload/catalog/controller/checkout/shipping_method.php
@@ -103,7 +103,7 @@ class ControllerCheckoutShippingMethod extends Controller {
 		}
 
 		// Validate cart has products and has stock.
-		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+		if ((!$this->cart->hasSellerproducts($seller_id) && empty($this->session->data['vouchers'])) || (!$this->cart->hasSellerstock($seller_id) && !$this->config->get('config_stock_checkout'))) {
 			$json['redirect'] = $this->url->link('checkout/cart');
 		}
 
