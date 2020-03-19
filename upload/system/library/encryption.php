312a2531ce5316943ea692254e9f87595df25157
arnisjuraga <arnis.juraga@gmail.com>
2020-03-19 11:40:25 +0200

    Fix #21 mcrypt extension removed
    Should work up to with PHP 7.4

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,16 +1,27 @@
 <?php
 final class Encryption {
+
+	private $cipher = 'aes-256-ctr';
+	private $digest = 'sha256';
 	private $key;
 
 	public function __construct($key) {
-		$this->key = hash('sha256', $key, true);
+		$this->key = $key;
 	}
 
 	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $value, MCRYPT_MODE_ECB)), '+/=', '-_,');
+		$key = openssl_digest($this->key, $this->digest, true);
+		$iv_length = openssl_cipher_iv_length($this->cipher);
+		$iv = openssl_random_pseudo_bytes($iv_length);
+		return base64_encode($iv . openssl_encrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv));
 	}
 
 	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB));
+		$key = openssl_digest($this->key, $this->digest, true);
+		$iv_length = openssl_cipher_iv_length($this->cipher);
+		$value = base64_decode($value);
+		$iv = substr($value, 0, $iv_length);
+		$value = substr($value, $iv_length);
+		return openssl_decrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
 	}
 }
